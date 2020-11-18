5aaec94747505ff9b431dc7b7a8ccd843cf7a4d2
Joseph Burke <joseph_burke@outlook.com>
2020-11-18 08:59:52 +0000

    Create Task constructor and test it

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 import './style/style.scss'
 
-const content = document.querySelector('.content');
-const title = document.createElement('h1');
-title.textContent = 'This is a test';
+function Task(title, description, dueDate, priority) {
+  this.title = title
+  this.description = description
+  this.dueDate = dueDate
+  this.priority = priority
+}
 
-content.appendChild(title);
+// Test code to check whether task details can be outputted into the DOM.
+
+let task1 = new Task('Task1', 'The first task', '1st December 2020', 'High Priority');
+
+let content = document.querySelector('.content');
+
+let taskCard = document.createElement('div');
+taskCard.classList.add('card', 'card-body');
+taskCard.innerText = `${task1.title} ${task1.description} ${task1.dueDate} ${task1.priority}`;
+
+content.appendChild(taskCard);
