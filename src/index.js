db32f5c0313e36427f7a75dd6e21ba35aa7b551d
Joseph Burke <joseph_burke@outlook.com>
2020-11-18 09:12:15 +0000

    Move Task and Project constructors into logic.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,3 @@
 import './style/style.scss'
+import { Task, Project } from './logic';
 
-function Task(title, description, dueDate, priority) {
-  this.title = title
-  this.description = description
-  this.dueDate = dueDate
-  this.priority = priority
-}
-
-// Test code to check whether task details can be outputted into the DOM.
-
-let task1 = new Task('Task1', 'The first task', '1st December 2020', 'High Priority');
-
-let content = document.querySelector('.content');
-
-let taskCard = document.createElement('div');
-taskCard.classList.add('card', 'card-body');
-taskCard.innerText = `${task1.title} ${task1.description} ${task1.dueDate} ${task1.priority}`;
-
-content.appendChild(taskCard);
